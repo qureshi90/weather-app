d261f7cc5cfeaa5d4616df7d3f636f6ac4d535de
Waqar Qureshi <waqarqureshi90@gmail.com>
2020-12-15 22:23:11 +0500

    api setup

--- src/App.js
+++ src/App.js
@@ -45,20 +45,18 @@ function App() {
           onKeyPress={search}
         />
       </div>
-      {(typeof weather.main != "undefined") ? (
-      <div>
-        <div className="location-box">
-          <div className="location">{weather.name}, {weather.sys.country}</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">
-            {Math.round(weather.main.temp)}°c
-          </div>
-          <div className="weather">{weather.weather[0].main}</div>
-        </div>
+
+      <div className="location-box">
+        <div className="location">{weather.name}, {weather.sys.country}</div>
+        <div className="date">{dateBuilder(new Date())}</div>
       </div>
-      ) : ('')}
+      
+      <div className="weather-box">
+        <div className="temp">
+          {Math.round(weather.main.temp)}°c
+        </div>
+        <div className="weather">{weather.weather[0].main}</div>
+      </div>
     </main>
   
   );
