b330c12f20490367a0c11353cf377e195e7a47ad
Waqar Qureshi <waqarqureshi90@gmail.com>
2020-12-16 15:31:13 +0500

    background added

--- src/App.js
+++ src/App.js
@@ -33,34 +33,34 @@ function App() {
   }
 
   return (
-    
-    <main>
-      <div className="search-box">
-        <input 
-          type="text"
-          className="search-bar"
-          placeholder="Search..."
-          onChange={e => setQuery(e.target.value)}
-          value={query}
-          onKeyPress={search}
-        />
-      </div>
-      {(typeof weather.main != "undefined") ? (
-      <div>
-        <div className="location-box">
-          <div className="location">{weather.name}, {weather.sys.country}</div>
-          <div className="date">{dateBuilder(new Date())}</div>
+    <div className="App">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-        <div className="weather-box">
-          <div className="temp">
-            {Math.round(weather.main.temp)}°c
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
           </div>
-          <div className="weather">{weather.weather[0].main}</div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
         </div>
-      </div>
-      ) : ('')}
-    </main>
-  
+        ) : ('')}
+      </main>
+    </div>
   );
 }
 
