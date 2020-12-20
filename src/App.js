7d8d2999962b4b6de934ac22d6ccecf1c2367659
Waqar Qureshi <waqarqureshi90@gmail.com>
2020-12-20 16:30:29 +0500

    fetch methods synchronized

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import humidity from './icons/humidity.png';
+import pressure from './icons/pressure.png';
+import wind from './icons/wind.png';
 
 const api = {
   key: "9462236f0e28049b29c2a923b7eb368d",
@@ -13,6 +16,23 @@ function App() {
 
   const search = (e) => {
     if (e.key === "Enter") {
+      
+      Promise.all([
+        fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`).then(value => value.json()),
+        fetch(`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}`).then(value => value.json())
+        ])
+        .then((values) => {
+          const value1 = values[0]
+          const value2 = values[1]
+          setWeather(value1);
+          setForecast(value2);
+          setQuery('');
+          setIcon(value1.weather[0].icon);
+          console.log(value1);
+          console.log(value2);
+        })
+      
+      /*
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
         .then(result => {
@@ -24,23 +44,24 @@ function App() {
       
       fetch(`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}`)
         .then(resp => resp.json())
-        .then(result1 => {
-          setForecast(result1);
-          console.log(result1);
+        .then(result => {
+          setForecast(result);
+          console.log(result);
         });
+      */
     }
   }
 
   const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
+    return `${day}, ${month} ${date}, ${year}`
   }
 
   return (
@@ -64,50 +85,68 @@ function App() {
           />
         </div>
 
-        {(typeof weather.main != "undefined") ? (
-          <div className="main-container">
+        {(typeof weather.main != "undefined") && (typeof forecast.city != "undefined") ? (
+          <div className="window" >
+            <div className="main-container">
 
-            <div className="left-container">
+              <div className="left-container">
+                <div>
+                  <img 
+                    src={ 'http://openweathermap.org/img/w/' + icon + '.png' } 
+                    alt="weather condition icon" 
+                    className="weather-icon"
+                  />
+                </div>
+
+                <div className="weather">
+                  {weather.weather[0].main}
+                </div>
+
+                <div className="temperature">
+                  {Math.round(weather.main.temp)}°C
+                </div>
+
+                <div className="location">
+                  {weather.name}, {weather.sys.country}
+                </div>
+
+                <div className="date">
+                  {dateBuilder(new Date())}
+                </div>
+              </div>
+
               <div>
-                <img 
-                  src={ 'http://openweathermap.org/img/w/' + icon + '.png' } 
-                  alt="weather condition icon" 
-                  className="weather-icon"
-                />
-              </div>
-
-              <div className="weather">
-                {weather.weather[0].main}
-              </div>
-
-              <div className="temperature">
-                {Math.round(weather.main.temp)}°C
-              </div>
-
-              <div className="location">
-                {weather.name}, {weather.sys.country}
-              </div>
-
-              <div className="date">
-                {dateBuilder(new Date())}
-              </div>
-            </div>
-
-            <div>
-              <img src="https://media.giphy.com/media/h7Y3rfqV9qADYcOJaD/giphy.gif" alt="gif icon"/>
-            </div>
-            
-            <div className="right-container">
-              <div className="humidity">
-                Humidity <br/> {weather.main.humidity}%
-              </div>
-
-              <div className="pressure">
-                Pressure <br/> {weather.main.pressure} mBar
+                <img src="https://media.giphy.com/media/h7Y3rfqV9qADYcOJaD/giphy.gif" alt="gif icon"/>
               </div>
               
-              <div className="wind">
-                Wind <br/> {Math.round((weather.wind.speed) * 15/8 )} km/h
+              <div className="right-container">
+                <div className="i-container">
+                  <img src={humidity} alt="humidity icon" className="icons" />
+                  <div className="humidity">
+                    Humidity <br/> {weather.main.humidity}%
+                  </div>
+                </div>
+
+                <div className="i-container">
+                  <img src={pressure} alt="pressure icon" className="icons" />
+                  <div className="pressure">
+                    Pressure <br/> {weather.main.pressure} mBar
+                  </div>
+                </div>
+
+                <div className="i-container">
+                  <img src={wind} alt="wind icon" className="icons" />
+                  <div className="wind">
+                    Wind <br/> {Math.round((weather.wind.speed) * 15/8 )} km/h
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            
+            <div className="lower-container">
+              <div className="zehr">
+              {Math.round(forecast.list[0].main.temp)}
               </div>
             </div>
 
